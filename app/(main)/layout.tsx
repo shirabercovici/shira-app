5ea2f216fcd2ace58d118d970f73889eed0728fb
shirabercovici <shira.bercovich@gmail.com>
2025-11-16 10:46:36 +0200

    moved the styles folder into the main folder

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,5 +1,6 @@
 import Footer from "../../lib/components/Footer";
 import NavBar from "../../lib/components/Navbar";
+import "./styles/global.css";
 
 export default function MainLayout({
   children,
