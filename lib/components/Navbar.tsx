8e111722cd1f76f8d4ea4875a547dacebc4de2a2
shirabercovici <shira.bercovich@gmail.com>
2025-11-17 13:04:10 +0200

    updated the navbar file name

--- lib/components/Navbar.tsx
+++ lib/components/Navbar.tsx
@@ -3,7 +3,7 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
-import styles from './NavBar.module.css';
+import styles from './Navbar.module.css';
 
 export default function NavBar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
